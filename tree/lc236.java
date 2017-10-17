42558a9559d824b3a995208eccfd728e3121e6d2
林楚 <true@linchudeMBP.wv.cc.cmu.edu>
2017-10-17 14:09:44 -0400

    lc236

---
+++ tree/lc236.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// from bottom to top. 
+// 1. pass the node to top.
+// 2. the first cross will contain the two nodes.
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return  root;
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        }
+        
+        if (left != null) {
+            return left;
+        }
+        
+        if (right != null) {
+            return right;
+        }
+        
+        return null;
+    }
+}
