dce962de84938b81f78e93c4e4b2283ed839c6d6
林楚 <true@linchudeMBP.wv.cc.cmu.edu>
2017-10-18 20:01:25 -0400

    lc94

---
+++ tree/lc94_inorder_traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Stack<TreeNode> myStack = new Stack<>();
+    List<Integer> myList = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return myList;
+        }
+        init(root);
+        while (hasNext()) {
+            TreeNode node = next();
+            myList.add(node.val);
+        }
+        return myList;
+    }
+    
+    private void init(TreeNode root) {
+        pushAll(root);
+    }
+    
+    private boolean hasNext() {
+        return !myStack.isEmpty();
+    }
+    
+    private TreeNode next() {
+        TreeNode nextNode = myStack.pop();
+        pushAll(nextNode.right);
+        return nextNode;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; node = node.left) {
+            myStack.push(node);
+        }
+    }
+}
